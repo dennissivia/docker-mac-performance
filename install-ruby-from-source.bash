765707cb87050e44aaca791fe32f3b4f6bbdc50b
Dennis Sivia <dennis.sivia@gmail.com>
2020-07-30 10:52:32 +0200

    Add debian setup scripts 🐧

---
+++ install-ruby-from-source.bash
@@ -0,0 +1,15 @@
+
+apt remove -y clang ruby gcc
+
+apt install -y wget software-properties-common make lsb-release gnupg2
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+apt update -y
+apt upgrade -y
+
+apt install -y clang-10
+
+wget https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.5.tar.gz
+tar -xvf ruby-2.5.5.tar.gz
+cd ruby-2.5.5 && CC=/usr/bin/clang-10 ./configure
+cd ruby-2.5.5 && make && make install
---
+++ prepare-debian.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt update -y; apt upgrade -y; apt install wget lsb-utils gnupg2 git software-properties-common make
