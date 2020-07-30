ef7d6138b871a34bd52ed4ee757116ed047f1fad
Dennis Sivia <dennis.sivia@gmail.com>
2020-07-30 11:41:18 +0200

    Fix command path

--- run-bench.bash
+++ run-bench.bash
@@ -20,8 +20,8 @@ sleep $PAUSE_TIME
 
 os=$( uname -s )
 if [ "${os}" = "Linux" ]; then
-echo "Linux: with manual compiled 2.5.5 (clang)"
-  hyperfine --min-runs 10 --warmup 2 '/ruby -e "GC.disable; 300_000_000.times{1+1}"'
+  echo "Linux: with manual compiled 2.5.5 (clang)"
+  hyperfine --min-runs 10 --warmup 2 'ruby -e "GC.disable; 300_000_000.times{1+1}"'
 else
   rvm use 2.5.5
   echo "native with ruby 2.5.5 (RVM)"
