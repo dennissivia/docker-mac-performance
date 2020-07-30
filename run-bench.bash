efacaaf674c1d3c3513335f2a82f57718f89cacf
Dennis Sivia <dennis.sivia@gmail.com>
2020-07-30 11:27:17 +0200

    Fix package name and make run-bench cross OS
    Fixes the lsb-release package name in the debian setup script.
    Adds a condition to run `run-bench.bash` directly on Linux servers.

--- run-bench.bash
+++ run-bench.bash
@@ -18,18 +18,24 @@ echo "Starting docker run debian with compiled ruby-2.5.5 (clang)"
 docker run -it --rm  --mount type=tmpfs,destination=/tmp ruby-bench:ruby-2.5.5-compiled
 sleep $PAUSE_TIME
 
-rvm use 2.5.5
-echo "native with ruby 2.5.5 (RVM)"
-hyperfine --min-runs 10 --warmup 2 'ruby -e "GC.disable; 300_000_000.times{1+1}"'
-sleep $PAUSE_TIME
+os=$( uname -s )
+if [ "${os}" = "Linux" ]; then
+echo "Linux: with manual compiled 2.5.5 (clang)"
+  hyperfine --min-runs 10 --warmup 2 '/ruby -e "GC.disable; 300_000_000.times{1+1}"'
+else
+  rvm use 2.5.5
+  echo "native with ruby 2.5.5 (RVM)"
+  hyperfine --min-runs 10 --warmup 2 'ruby -e "GC.disable; 300_000_000.times{1+1}"'
+  sleep $PAUSE_TIME
 
-echo "native with ruby 2.6.5 (RVM)"
-rvm use 2.6.5
-hyperfine --min-runs 10 --warmup 2 'ruby -e "GC.disable; 300_000_000.times{1+1}"'
-sleep $PAUSE_TIME
+  echo "native with ruby 2.6.5 (RVM)"
+  rvm use 2.6.5
+  hyperfine --min-runs 10 --warmup 2 'ruby -e "GC.disable; 300_000_000.times{1+1}"'
+  sleep $PAUSE_TIME
 
-echo "with manual compiled 2.5.5 (clang)"
-cd ~/dev/private/docker-mac-performance/ruby-src/ruby-2.5.5
-hyperfine --min-runs 10 --warmup 2 './ruby -I $PWD/lib -I $PWD -I $PWD/ext -I .ext/x86_64-darwin19 -e "GC.disable; 300_000_000.times{1+1}"'
+  echo "with manual compiled 2.5.5 (clang)"
+  cd ~/dev/private/docker-mac-performance/ruby-src/ruby-2.5.5
+  hyperfine --min-runs 10 --warmup 2 './ruby -I $PWD/lib -I $PWD -I $PWD/ext -I .ext/x86_64-darwin19 -e "GC.disable; 300_000_000.times{1+1}"'
 
-echo "done."
+  echo "done."
+fi
