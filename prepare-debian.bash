99492ba3a42204afffeda9fe973a11dcc7f9a49e
Dennis Sivia <dennis.sivia@gmail.com>
2020-07-30 11:00:42 +0200

    Add hyperfine installation to debian setup script

--- prepare-debian.bash
+++ prepare-debian.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-apt update -y; apt upgrade -y; apt install wget lsb-utils gnupg2 git software-properties-common make
+apt update -y; apt upgrade -y; apt install wget lsb-utils gnupg2 git software-properties-common make docker.io
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.10.0/hyperfine_1.10.0_amd64.deb && dpkg -i hyperfine_1.10.0_amd64.deb
